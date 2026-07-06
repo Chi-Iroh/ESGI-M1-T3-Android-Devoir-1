69ab1e61e2972e028c753e90dfc53194c1ad266e
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2026-07-06 19:09:45 +0200

    Correction de la taille du texte

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.profile.ui.theme.ProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,17 +37,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Name(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        modifier = modifier.height(24.dp)
-    )
+fun Name(name: String, occupation: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = name,
+            modifier = modifier,
+            fontSize = 24.sp
+        )
+        Text(
+            text = occupation,
+            modifier = modifier,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Composable
 fun Profile(name: String, modifier: Modifier = Modifier) {
     Surface(modifier = modifier) {
-        Name(name = name, modifier = modifier)
+        Name(name = name, "Développeur C & C++", modifier = modifier)
     }
 }
 
