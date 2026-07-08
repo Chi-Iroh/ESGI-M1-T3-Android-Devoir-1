4701a477581d837f681b69ea1ee233beabe7c562
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2026-07-08 10:11:34 +0200

    Ajout d'une LazyColumn pour les compétences

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -66,10 +70,20 @@ fun SkillItem(skill: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun Skills(skills: List<String>, modifier: Modifier = Modifier) {
-    Text(
-        text = "Compétences :"
-    )
-    skills.forEach { skill -> SkillItem(skill, modifier) }
+        LazyColumn (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+            item {
+                Text(
+                    text = "Compétences :"
+                )
+            }
+
+            items(items = skills) {
+                item -> SkillItem(item, modifier)
+            }
+    }
 }
 
 @Composable
